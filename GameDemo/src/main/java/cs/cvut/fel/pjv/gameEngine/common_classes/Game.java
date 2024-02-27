86c185158e18aad4331f07e19440445286a6275e
Eleonora Virych <virycele@fel.cvut.cz>
2024-02-27 10:29:39 +0100

    update Game methods saveGame() and loadGame()

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gameEngine/common_classes/Game.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gameEngine/common_classes/Game.java
@@ -1,6 +1,7 @@
 public class Game {
     private Player player;
-    private currentWagon;
+    private Wagon currentWagon;
+    private Train train;
 
     public Game(Player player) {
         this.player = player;
@@ -23,11 +24,35 @@ public class Game {
         // save Train class
         //save Player class
         //save currentWagon
+        try {
+            FileOutputStream fileOut = new FileOutputStream("game.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
     }
 
     public void loadGame() {
         // load Train class
         // load Player class
+        // load currentWagon
+        try {
+            FileInputStream fileIn = new FileInputStream("game.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Game game = (Game) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Game class not found");
+            c.printStackTrace();
+            return;
+        }
     }
 
     public void startGame() {
