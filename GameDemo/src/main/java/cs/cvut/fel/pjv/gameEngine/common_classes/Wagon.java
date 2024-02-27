fbe36a34cbb06fc88f6e937a90f404ee00b60d93
Eleonora Virych <virycele@fel.cvut.cz>
2024-02-27 10:22:30 +0100

    class Wagon added

---
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gameEngine/common_classes/Wagon.java
@@ -0,0 +1,52 @@
+public class Wagon {
+    public final int id;
+    public final String type;
+    public final String texturePath;
+    private Door doorLeft;
+    private Door doorRight;
+    private Entity[] entitiesArray;
+    private Object[] objectsArray;
+
+    public Wagon(int id, String type, String texturePath) {
+        this.id = id;
+        this.type = type;
+        this.texturePath = texturePath;
+    }
+
+    public Wagon(int id, String type, String texturePath, Door doorLeft, Door doorRight, Entity[] entitiesArray, Object[] objectsArray) {
+        this.id = id;
+        this.type = type;
+        this.texturePath = texturePath;
+        this.doorLeft = doorLeft;
+        this.doorRight = doorRight;
+        this.entitiesArray = entitiesArray;
+        this.objectsArray = objectsArray;
+    }
+
+    public void setDoorLeft(Door doorLeft) {
+        this.doorLeft = doorLeft;
+    }
+
+    public void setDoorRight(Door doorRight) {
+        this.doorRight = doorRight;
+    }
+
+    public void setEntitiesArray(Entity[] entitiesArray) {
+        this.entitiesArray = entitiesArray;
+    }
+
+    public void getEntitiesArray() {
+        return entitiesArray;
+    }
+
+    public void setObjectsArray(Object[] objectsArray) {
+        this.objectsArray = objectsArray;
+    }
+
+    public void getObjectsArray() {
+        return objectsArray;
+    }
+
+    public void generateWagon(String seed) {
+        // Generate the wagon
+    }
