67010d33bf5c2febae3d1c063c770b024025cda7
Eleonora Virych <virycele@fel.cvut.cz>
2024-02-27 10:22:01 +0100

    class Train added

---
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gameEngine/common_classes/Train.java
@@ -0,0 +1,37 @@
+public class Train {
+    public final Wagon[] wagonsArray[Constants.TRAIN_WAGONS];
+
+    public Train(Wagon[] wagonsArray) {
+        this.wagonsArray = wagonsArray;
+    }
+
+    public void addWagon(Wagon wagon) {
+        for (int i = 0; i < wagonsArray.length; i++) {
+            if (wagonsArray[i] == null) {
+                wagonsArray[i] = wagon;
+                return;
+            }
+        }
+        //if no empty space is found, remove the oldest wagon (with the lowest id)
+        removeWagon(wagonsArray[findMinId()]);
+        addWagon(wagon);
+    }
+
+    public void removeWagon(Wagon wagon) {
+        for (int i = 0; i < wagonsArray.length; i++) {
+            if (wagonsArray[i] == wagon) {
+                wagonsArray[i] = null;
+            }
+        }
+    }
+
+    private int findMinId() {
+        int minId = wagonsArray[0].id;
+        for (int i = 1; i < wagonsArray.length; i++) {
+            if (wagonsArray[i].id < minId) {
+                minId = wagonsArray[i].id;
+            }
+        }
+        return minId;
+    }
+}
