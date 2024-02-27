11b550bacc30b02176e411f05792f9d81681ea11
Eleonora Virych <virycele@fel.cvut.cz>
2024-02-27 10:21:31 +0100

    class Constants added

---
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gameEngine/common_classes/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static final int PLAYER_START_POS_X = 0;
+    public static final int PLAYER_START_POS_Y = 0;
+    public static final int PLAYER_BASIC_SPEED_X = 5;
+    public static final int PLAYER_BASIC_SPEED_Y = 5;
+    public static final int PLAYER_BASIC_DAMAGE = 5;
+    public static final int PLAYER_HITBOX = 1;
+    public static final int PLAYER_MAX_HEALTH = 100;
+    public static final int PLAYER_MAX_HUNGER = 100;
+    public static final int PLAYER_INVENTORY_SIZE = 15;
+    public static final int ENTITY_BASIC_SPEED_X = 4;
+    public static final int ENTITY_BASIC_SPEED_Y = 4;
+    public static final int TRAIN_WAGONS = 10;
+}
