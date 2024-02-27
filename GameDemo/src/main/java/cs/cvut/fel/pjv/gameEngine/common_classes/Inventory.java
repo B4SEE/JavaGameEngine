b37d7ceb372ed173c3befad460dffb65c972be6e
Eleonora Virych <virycele@fel.cvut.cz>
2024-02-27 09:19:59 +0100

    class Inventory added

---
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gameEngine/common_classes/Inventory.java
@@ -0,0 +1,53 @@
+public class Inventory {
+    public final inventorySize;
+    private Item[] itemsArray;
+
+    public Inventory(int size) {
+        inventorySize = size;
+        itemsArray = new Item[size];
+    }
+
+    public void openInventory() {
+        // Open the inventory
+    }
+
+    public void closeInventory() {
+        // Close the inventory
+    }
+
+    public void addItem(Item item) {
+        if (item == null) {
+            return;
+        }
+        for (int i = 0; i < inventorySize; i++) {
+            if (itemsArray[i] == null) {
+                itemsArray[i] = item;
+                return;
+            }
+        }
+        // Inventory is full
+        return;
+    }
+
+    public void removeItem(Item item) {
+        if (item == null) {
+            return;
+        }
+        for (int i = 0; i < inventorySize; i++) {
+            if (itemsArray[i] == item) {
+                itemsArray[i] = null;
+                return;
+            }
+        }
+        // Item not found
+        return;
+    }
+
+    public void setItemsArray(Item[] items) {
+        itemsArray = items;
+    }
+
+    public Item[] getItemsArray() {
+        return itemsArray;
+    }
+}
