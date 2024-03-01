05e9dcdf6402379a79c408bfe775b4f38a6abbf6
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-01 11:12:54 +0100

    update Constants, object ids added

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Constants.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Constants.java
@@ -1,4 +1,7 @@
 package cs.cvut.fel.pjv.gamedemo.common_classes;
+
+import java.util.Map;
+
 public class Constants {
     public static final int PLAYER_START_POS_X = 0;
     public static final int PLAYER_START_POS_Y = 0;
@@ -12,4 +15,22 @@ public class Constants {
     public static final int ENTITY_BASIC_SPEED_X = 4;
     public static final int ENTITY_BASIC_SPEED_Y = 4;
     public static final int TRAIN_WAGONS = 10;
+
+    public static final Map <String, String> OBJECT_IDS = Map.of(
+            "BB", "0",
+            "BG", "0",
+            "TF", "tile_floor.png",
+            "SW", "block_wall.png",
+            "HW", "block_wagon_wall.png",
+            "WW", "3h_block.png"
+    );
+
+    public static final Map <String, String> OBJECT_NAMES = Map.of(
+            "BB", "blank_0",
+            "BG", "blank_gap_0",
+            "TF", "floor_0",
+            "SW", "wall_1",
+            "HW", "wagon_wall_2",
+            "WW", "wagon_wall_3"
+    );
 }
