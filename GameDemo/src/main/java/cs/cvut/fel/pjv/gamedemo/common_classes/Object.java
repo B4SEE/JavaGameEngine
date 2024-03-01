529cb7af6fe3f494a862de55687d85c658856510
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-01 11:14:37 +0100

    update Object, attributes twoLetterId, height, cartX and cartY added; texturePath is not final now

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Object.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Object.java
@@ -1,8 +1,13 @@
 package cs.cvut.fel.pjv.gamedemo.common_classes;
 public class Object {
     public final int id;
+
     public final String name;
-    public final String texturePath;
+    public String texturePath;
+    private String twoLetterId;
+    private int height;
+    private int cartX;
+    private int cartY;
     private int gridPositionX;
     private int gridPositionY;
     private boolean isSolid;
@@ -14,6 +19,17 @@ public class Object {
         this.texturePath = texturePath;
     }
 
+    public Object(int id, String name, String texturePath, String twoLetterId, int height, int cartX, int cartY, boolean isSolid) {
+        this.id = id;
+        this.name = name;
+        this.texturePath = texturePath;
+        this.twoLetterId = twoLetterId;
+        this.height = height;
+        this.cartX = cartX;
+        this.cartY = cartY;
+        this.isSolid = isSolid;
+    }
+
     public Object(int id, String name, String texturePath, int gridPositionX, int gridPositionY, boolean isSolid, Inventory objectInventory) {
         this.id = id;
         this.name = name;
@@ -22,6 +38,44 @@ public class Object {
         this.gridPositionY = gridPositionY;
         this.isSolid = isSolid;
         this.objectInventory = objectInventory;
+    }
+    public void setTexturePath(String texturePath) {
+        this.texturePath = texturePath;
+    }
+    public String getTexturePath() {
+        return texturePath;
+    }
+
+    public void setTwoLetterId(String twoLetterId) {
+        this.twoLetterId = twoLetterId;
+    }
+
+    public String getTwoLetterId() {
+        return twoLetterId;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setCartX(int cartX) {
+        this.cartX = cartX;
+    }
+
+    public int getCartX() {
+        return cartX;
+    }
+
+    public void setCartY(int cartY) {
+        this.cartY = cartY;
+    }
+
+    public int getCartY() {
+        return cartY;
     }
 
     public void setGridPositionX(int gridPositionX) {
