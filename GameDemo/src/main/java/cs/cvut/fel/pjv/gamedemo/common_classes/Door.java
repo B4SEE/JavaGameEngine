59c5a27b576812caa4c16b79bca5cdb5cf784e21
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-07 11:31:53 +0100

    Door now extends Object, int targetId changed to int[] targetId_teleportX_teleportY

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Door.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Door.java
@@ -1,68 +1,22 @@
 package cs.cvut.fel.pjv.gamedemo.common_classes;
-public class Door {
-    public final String name;
-    private String twoLetterId;
-    public String texturePath;
-    private int gridPositionX;
-    private int gridPositionY;
-    private int targetId;
+public class Door extends Object {
+    private int[] targetId_teleportX_teleportY;
     private boolean isLocked;
-
-    public Door(String name) {
-        this.name = name;
-    }
-    public Door(String name, String twoLetterId, String texturePath, int gridPositionX, int gridPositionY, int targetId, boolean isLocked) {
-        this.name = name;
-        this.twoLetterId = twoLetterId;
-        this.texturePath = texturePath;
-        this.gridPositionX = gridPositionX;
-        this.gridPositionY = gridPositionY;
-        this.targetId = targetId;
+    public Door(int id, String name, String texturePath, int[] targetId_teleportX_teleportY, boolean isLocked) {
+        super(id, name, texturePath);
+        super.setObjectInventory(null);
+        super.setHeight(1);
+        super.setIsSolid(true);
+        this.targetId_teleportX_teleportY = targetId_teleportX_teleportY;
         this.isLocked = isLocked;
     }
 
-    public String getName() {
-        return name;
+    public int[] getTargetId_teleportX_teleportY() {
+        return targetId_teleportX_teleportY;
     }
 
-    public String getTwoLetterId() {
-        return twoLetterId;
-    }
-
-    public void setTwoLetterId(String twoLetterId) {
-        this.twoLetterId = twoLetterId;
-    }
-
-    public String getTexturePath() {
-        return texturePath;
-    }
-
-    public void setTexturePath(String texturePath) {
-        this.texturePath = texturePath;
-    }
-
-    public void setGridPositionX(int gridPositionX) {
-        this.gridPositionX = gridPositionX;
-    }
-
-    public int getGridPositionX() {
-        return gridPositionX;
-    }
-
-    public void setGridPositionY(int gridPositionY) {
-        this.gridPositionY = gridPositionY;
-    }
-
-    public int getGridPositionY() {
-        return gridPositionY;
-    }
-
-    public void setTargetId(int targetId) {
-        this.targetId = targetId;
-    }
-
-    public int getTargetId() {
-        return targetId;
+    public void setTargetId_teleportX_teleportY(int[] targetId_teleportX_teleportY) {
+        this.targetId_teleportX_teleportY = targetId_teleportX_teleportY;
     }
 
     public void teleport(Entity entity) {
