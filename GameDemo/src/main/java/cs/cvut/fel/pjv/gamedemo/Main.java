996e77bce1c427dd7d462d25a13c4ea864df3c44
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-12 11:09:54 +0100

    update Main

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
@@ -1,14 +1,9 @@
 package cs.cvut.fel.pjv.gamedemo;
 
-import cs.cvut.fel.pjv.gamedemo.IsometricEngine.Isometric;
-import cs.cvut.fel.pjv.gamedemo.common_classes.Constants;
-import cs.cvut.fel.pjv.gamedemo.common_classes.Entity;
-import cs.cvut.fel.pjv.gamedemo.common_classes.Player;
+import cs.cvut.fel.pjv.gamedemo.common_classes.*;
+import cs.cvut.fel.pjv.gamedemo.engine.GameLogic;
+import cs.cvut.fel.pjv.gamedemo.engine.Isometric;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,20 +15,21 @@ public class Main extends Application {
         player.setHitBoxSize(1);
         player.setAttackRangeSize(1);
         player.setHealth(Constants.PLAYER_MAX_HEALTH);
-        Entity entity = new Entity(1, "ENTITY_NAME", "player_front.png", "NEUTRAL", 0, 0, 1, 100, 1, null);
+        Entity entity = new Entity(1, "ENTITY_NAME", "player_front.png", "NEUTRAL", 0, 0, 1, 100, 10, null);
         entity.setHeight(2);
         entity.setHitBoxSize(1);
         entity.setAttackRangeSize(1);
-//        entity.setPositionX(100);
-//        entity.setPositionY(100);
+        entity.setWhenAttacked(0);
+        entity.setCooldown(0);
+        entity.setInitialType(entity.getType());
         Entity[] entities = {entity};
-        Isometric isometric = new Isometric();
-        isometric.initialiseStage(stage);
-        isometric.setPlayer(player);
-        isometric.setEntities(entities);
-        isometric.setMap("maps/test_map_3.txt");
-//        isometric.previewMap();
-        isometric.start();
+//        Inventory inventory = new Inventory(50);
+//        Item item = new Item(0, "ITEM_NAME", "block_wall.png");
+//        inventory.addItem(item);
+//        inventory.openInventory(stage);
+//        inventory.closeInventory(stage);
+        GameLogic gameLogic = new GameLogic(stage);
+        gameLogic.loadGame(player, entities, "maps/test_map_3.txt");
     }
 
     public static void main(String[] args) {
