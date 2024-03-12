b7c6ef2f9bdb827fc7805114dd14f8856697c564
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-12 11:12:34 +0100

    update Inventory, openInventory and closeInventory methods were implemented

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Inventory.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Inventory.java
@@ -1,4 +1,11 @@
 package cs.cvut.fel.pjv.gamedemo.common_classes;
+
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 public class Inventory {
     public final int inventorySize;
     private Item[] itemsArray;
@@ -8,12 +15,31 @@ public class Inventory {
         itemsArray = new Item[size];
     }
 
-    public void openInventory() {
-        // Open the inventory
+    public void openInventory(Stage stage) {
+        GridPane grid = new GridPane();
+        grid.setStyle("-fx-background-color: #000000; -fx-border-color: #ffffff;");
+        grid.setPrefSize(800, 800);
+        //add style to grid lines
+        grid.setHgap(10);
+        grid.setVgap(10);
+        for (int i = 0; i < inventorySize; i++) {
+            if (itemsArray[i] != null) {
+                ImageView imageView = new ImageView(itemsArray[i].texturePath);
+                imageView.setFitHeight(50);
+                imageView.setFitWidth(50);
+                grid.add(imageView, i % 5, i / 5);
+            } else {
+                Rectangle rectangle = new Rectangle(50, 50);
+                rectangle.setStyle("-fx-fill: #af0303; -fx-stroke: #490606;");
+                grid.add(rectangle, i % Constants.INVENTORY_WIDTH, i / Constants.INVENTORY_WIDTH);
+            }
+        }
+        grid.setGridLinesVisible(true);
+        Scene inventoryScene = new Scene(grid, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        stage.setScene(inventoryScene);
     }
-
-    public void closeInventory() {
-        // Close the inventory
+    public void closeInventory(Stage stage) {
+        stage.setScene(null);
     }
 
     public void addItem(Item item) {
