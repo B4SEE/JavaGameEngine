b4790d4e92e86f34541703d2539e9a811296deff
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-12 14:34:46 +0100

    update Main

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
@@ -11,26 +11,32 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Create a new player
         Player player = new Player(0, "PLAYER_NAME", "player_front.png", 400, 0);
         player.setHitBoxSize(1);
         player.setAttackRangeSize(1);
-        player.setHealth(Constants.PLAYER_MAX_HEALTH);
-        Entity entity = new Entity(1, "ENTITY_NAME", "player_front.png", "ENTITY", 0, 0, 1, 100, 10, null);
+        player.setCooldown(2);
+        // Create a new entity
+        Entity entity = new Entity(1, "ENTITY_NAME", "player_front.png", "ENTITY", 50, 50, 1, 100, 3, null);
         entity.setHeight(2);
         entity.setHitBoxSize(1);
         entity.setAttackRangeSize(1);
         entity.setWhenAttacked(0);
-        entity.setCooldown(0);
+        entity.setCooldown(3);
         entity.setBehaviour("NEUTRAL");
         entity.setInitialBehaviour(entity.getBehaviour());
         Entity[] entities = {entity};
+
 //        Inventory inventory = new Inventory(50);
 //        Item item = new Item(0, "ITEM_NAME", "block_wall.png");
 //        inventory.addItem(item);
 //        inventory.openInventory(stage);
 //        inventory.closeInventory(stage);
+
+        // Load and start the game
         GameLogic gameLogic = new GameLogic(stage);
         gameLogic.loadGame(player, entities, "maps/test_map_3.txt");
+        gameLogic.start();
     }
 
     public static void main(String[] args) {
