8f31610d89234683a4bf2454467f00a5307ef0d4
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-23 17:57:30 +0100

    update files

--- GameDemo/src/main/java/module-info.java
+++ GameDemo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cs.cvut.fel.pjv.gamedemo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
 
     opens cs.cvut.fel.pjv.gamedemo to javafx.fxml;
