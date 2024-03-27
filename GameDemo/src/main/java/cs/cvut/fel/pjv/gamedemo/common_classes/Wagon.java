0d4858f53055890bf758b1a44120c622da4d318b
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-27 18:54:48 +0100

    update Wagon, method parseMap and loadMap moved to MapLoader class

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Wagon.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Wagon.java
@@ -1,6 +1,7 @@
 package cs.cvut.fel.pjv.gamedemo.common_classes;
 
 import cs.cvut.fel.pjv.gamedemo.engine.Checker;
+import cs.cvut.fel.pjv.gamedemo.engine.MapLoader;
 import cs.cvut.fel.pjv.gamedemo.engine.RandomHandler;
 
 import java.io.File;
@@ -124,15 +125,14 @@ public class Wagon {
     public void generateWagon() {
         // Generate the wagon
         RandomHandler randomHandler = new RandomHandler();
+        MapLoader mapLoader = new MapLoader();
         String path = randomHandler.getRandomWagonTypeLayout(type);
-        String seed = load(path);
-        parseMap(seed);
+        String unparsedSeed = mapLoader.load(path);
+        seed = mapLoader.parseMap(unparsedSeed);
+        initWagon();
     }
     public void generateNextWagon(Wagon wagon, boolean leftWagon) {//wagon - current wagon; leftWagon - if next wagon will be on the left side of the current wagon
-        RandomHandler randomHandler = new RandomHandler();
-        String path = randomHandler.getRandomWagonTypeLayout(type);
-        String seed = load(path);
-        parseMap(seed);
+        generateWagon();
 
         System.out.println(wagon.getDoorLeftTarget().getIsoX() + " " + wagon.getDoorLeftTarget().getIsoY());
         System.out.println(wagon.getDoorRightTarget().getIsoX() + " " + wagon.getDoorRightTarget().getIsoY());
@@ -153,76 +153,6 @@ public class Wagon {
             wagon.getDoorLeft().setTargetId(this.id);
             wagon.getDoorLeft().setTeleport(this.doorRightTarget);
         }
-    }
-    public void setWagonMap(String path) {
-        String seed = load(path);
-        parseMap(seed);
-    }
-
-    /**
-     * Load the map from the file (path)
-     * @param path - path to the file
-     * @return - the map
-     */
-    private String load(String path) {
-        File file = new File(path);
-        StringBuilder map = new StringBuilder();
-        try {
-            java.util.Scanner scanner = new java.util.Scanner(file);
-            while (scanner.hasNextLine()) {
-                map.append(scanner.nextLine());
-                map.append(Constants.MAP_ROW_SEPARATOR);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return map.toString();
-    }
-
-    /**
-     * Parse the map; replace the random objects with the actual objects.
-     * @param seed - the map
-     */
-    private void parseMap(String seed) {
-        try {
-            String[] rows = seed.split(Constants.MAP_ROW_SEPARATOR);
-            String[] subRows;
-            //3 - random; next number: 1 - chest, 2 - trap, 3 - enemy; next two numbers: chance; Note: was tested and works
-            for (int i = 0; i < rows.length; i++) {
-                subRows = rows[i].split(Constants.MAP_COLUMN_SEPARATOR);
-                for (int j = 0; j < subRows.length; j++) {
-                    if (subRows[j].charAt(0) == Constants.RANDOM) {
-                        int chance = Integer.parseInt(subRows[j].substring(2, 4));
-                        int generate = (int) (Math.random() * 100);
-                        if (chance > generate) {
-                            if (subRows[j].charAt(1) == '1') {
-                                // Generate the chest
-                                //generate random number from 1 to 9
-                                int random = (int) (Math.random() * 9 + 1);
-                                String chest = "2" + random + "CO";
-                                System.out.println(chest);
-                                subRows[j] = chest;
-                            } else if (subRows[j].charAt(1) == '2') {
-                                // Generate the trap
-                                String trap = "00TR";
-                                subRows[j] = trap;
-                            } else if (subRows[j].charAt(1) == '3') {
-                                // Generate the enemy
-                                String enemy = "00EN";
-                                subRows[j] = enemy;
-                            }
-                        } else {
-                            subRows[j] = "00TF";
-                        }
-                    }
-                }
-                rows[i] = String.join(Constants.MAP_COLUMN_SEPARATOR, subRows);
-            }
-            this.seed = String.join(Constants.MAP_ROW_SEPARATOR, rows);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        initWagon();
     }
 
     /**
