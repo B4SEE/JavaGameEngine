048068e4f1bff60f33cf674391617d697bc18e1b
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-27 14:25:06 +0100

    update Vendor, minor changes

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Vendor.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/common_classes/Vendor.java
@@ -3,6 +3,11 @@ package cs.cvut.fel.pjv.gamedemo.common_classes;
 public class Vendor extends Entity {
     private Inventory vendorInventory;
 
+
+    public Vendor(String name, String texturePath) {
+        super(name, texturePath);
+        setAsDefaultVendor();
+    }
     public Vendor(String name, String texturePath, int inventorySize) {
         super(name, texturePath);
         super.setAsDefaultNPC();
@@ -16,5 +21,14 @@ public class Vendor extends Entity {
 
     public void setVendorInventory(Inventory vendorInventory) {
         this.vendorInventory = vendorInventory;
+    }
+    public void setAsDefaultVendor() {
+        super.setAsDefaultNPC();
+        if (vendorInventory == null) {
+            int randomInventorySize = (int) (Math.random() * 20 + 1);
+            this.vendorInventory = new Inventory(randomInventorySize);
+            vendorInventory.setVendor(true);
+            vendorInventory.setInventoryLabel(this.getName());
+        }
     }
 }
