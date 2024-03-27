f1499a4c59f8cefc1a2defc104b395a471acdb64
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-27 14:23:11 +0100

    update Main, minor changes

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
@@ -1,12 +1,16 @@
 package cs.cvut.fel.pjv.gamedemo;
 
 import cs.cvut.fel.pjv.gamedemo.common_classes.*;
+import cs.cvut.fel.pjv.gamedemo.engine.Checker;
 import cs.cvut.fel.pjv.gamedemo.engine.Dialogue;
 import cs.cvut.fel.pjv.gamedemo.engine.GameLogic;
+import cs.cvut.fel.pjv.gamedemo.engine.RandomHandler;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main extends Application {
     @Override
@@ -14,7 +18,24 @@ public class Main extends Application {
         Player player = createPlayer();
         // Load and start the game
         GameLogic gameLogic = new GameLogic(stage);
-        gameLogic.loadGame(player, new Wagon(0, "DEFAULT", "wall_3.png"));
+        Wagon wagon = new Wagon(0, "DEFAULT", "wall_3.png");
+        Vendor vendor = new Vendor("vendor_test", "zombie_front.png", 10);
+        vendor.setPositionX(300);
+        vendor.setPositionY(240);
+        vendor.setNegativeThreshold(1);
+        Food item = new Food("orange", "orange.png", 15);
+        item.setValue(10);
+        MeleeWeapon item2 = new MeleeWeapon("knife", "knife.png", 20, 1);
+        item2.setValue(50);
+//        Item item3 = new Item("box", "chest_object_1.png");
+        Firearm item4 = new Firearm("gun", "chest_object_1.png", 10, 1);
+        item4.setValue(100);
+        vendor.getVendorInventory().addItem(item);
+        vendor.getVendorInventory().addItem(item2);
+        vendor.getVendorInventory().addItem(item4);
+        wagon.generateWagon();
+        wagon.addEntity(vendor);
+        gameLogic.loadGame(player, wagon);
         gameLogic.start();
     }
 
@@ -25,18 +46,16 @@ public class Main extends Application {
         player.setAttackRangeSize(1);
         player.setCooldown(2);
 
-        Food item = new Food("orange", "orange.png", 15);
-        MeleeWeapon item2 = new MeleeWeapon("knife", "knife.png", 20, 1);
-        Item item3 = new Item("box", "chest_object_1.png");
-
         PlayerInventory playerInventory = new PlayerInventory();
-
-        playerInventory.addItem(item);
-        playerInventory.addItem(item2);
-        playerInventory.addItem(item3);
+        RandomHandler randomHandler = new RandomHandler();
+        playerInventory.addItem(randomHandler.getRandomDefaultItem());
+        playerInventory.addItem(randomHandler.getRandomFoodItem());
+        playerInventory.addItem(randomHandler.getRandomMeleeItem());
+        playerInventory.addItem(randomHandler.getRandomFirearmItem());
 
         player.setPlayerInventory(playerInventory);
-        player.getPlayerInventory().setMoney(60);
+        player.getPlayerInventory().setMoney(160);
+        player.getPlayerInventory().setAmmo(50);
         return player;
     }
 
