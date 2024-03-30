e1eb212db41baa7c385cf62f03544bd054171247
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-30 22:52:47 +0100

    update module-info.java

--- GameDemo/src/main/java/module-info.java
+++ GameDemo/src/main/java/module-info.java
@@ -3,8 +3,10 @@ module cs.cvut.fel.pjv.gamedemo {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
 
-
     opens cs.cvut.fel.pjv.gamedemo to javafx.fxml;
+    opens cs.cvut.fel.pjv.gamedemo.common_classes to com.fasterxml.jackson.databind;
+
     exports cs.cvut.fel.pjv.gamedemo;
     exports cs.cvut.fel.pjv.gamedemo.engine;
+    exports cs.cvut.fel.pjv.gamedemo.common_classes to com.fasterxml.jackson.databind;
 }
