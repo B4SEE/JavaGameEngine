5936d39fe74d3e6b6f7eb6774f76fc89fde42459
Eleonora Virych <virycele@fel.cvut.cz>
2024-03-31 16:17:15 +0200

    new attributes&methods added

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/engine/Events.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/engine/Events.java
@@ -11,6 +11,9 @@ import java.util.List;
 public class Events {
     private static List<QuestNPC> availableQuestNPCs = new java.util.ArrayList<>();
     private static Constants.Event currentEvent;
+    private static boolean canSpawnLockedDoor = false;
+    private static int timeLoopCounter = 0;
+    private static Constants.Event nextEvent;
     public static List<QuestNPC> getAvailableQuestNPCs() {
         return availableQuestNPCs;
     }
@@ -26,5 +29,32 @@ public class Events {
 
     public static void setCurrentEvent(Constants.Event currentEvent) {
         Events.currentEvent = currentEvent;
+    }
+
+    public static boolean canSpawnLockedDoor() {
+        return canSpawnLockedDoor;
+    }
+
+    public static void setCanSpawnLockedDoor(boolean canSpawnLockedDoor) {
+        Events.canSpawnLockedDoor = canSpawnLockedDoor;
+    }
+
+    public static int getTimeLoopCounter() {
+        return timeLoopCounter;
+    }
+
+    public static void decrementTimeLoopCounter() {
+        timeLoopCounter--;
+    }
+    public static void setTimeLoopCounter(int timeLoopCounter) {
+        Events.timeLoopCounter = timeLoopCounter;
+    }
+
+    public static Constants.Event getNextEvent() {
+        return nextEvent;
+    }
+
+    public static void setNextEvent(Constants.Event nextEvent) {
+        Events.nextEvent = nextEvent;
     }
 }
