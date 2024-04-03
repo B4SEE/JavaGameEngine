ef084b9eaea8dfbf71fd4b95fb44f926235591e6
Eleonora Virych <virycele@fel.cvut.cz>
2024-04-03 16:51:18 +0200

    small update

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/Main.java
@@ -1,24 +1,19 @@
 package cs.cvut.fel.pjv.gamedemo;
 
-import cs.cvut.fel.pjv.gamedemo.common_classes.*;
-import cs.cvut.fel.pjv.gamedemo.engine.*;
+import cs.cvut.fel.pjv.gamedemo.engine.Atmospheric;
+import cs.cvut.fel.pjv.gamedemo.engine.Game;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Atmospheric.updateBackgroundMusic();
-
         // Load and start the game
-        Game game = new Game();
-        game.setStage(stage);
-        game.loadGame();
-        game.startGame();
+        Game.setStage(stage);
+        Game.start();
     }
 
     public static void main(String[] args) {
