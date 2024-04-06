908c7d30eaaf62a0b5f45c0fed196f1c9f1b847c
Eleonora Virych <virycele@fel.cvut.cz>
2024-04-06 22:39:49 +0200

    new methods added

--- GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/engine/Atmospheric.java
+++ GameDemo/src/main/java/cs/cvut/fel/pjv/gamedemo/engine/Atmospheric.java
@@ -1,18 +1,20 @@
 package cs.cvut.fel.pjv.gamedemo.engine;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
 import java.io.File;
 
 public class Atmospheric {
-    private static Clip clip;
-    private static float normalVolume;
+    private static MediaPlayer backgroundMusic;
+    private static final double normalVolume = 0.5;
     public static void updateBackgroundMusic() {
         int chance = (int) (Math.random() * 100);
         //check if music is playing
-        if (clip != null) {
+        if (backgroundMusic != null) {
             return;//do not play music if it is already playing
         }
         if (chance < 100) {
@@ -21,90 +23,116 @@ public class Atmospheric {
             File musicFile = RandomHandler.getRandomMusicFile("background/");
             //play music
             playMusic(musicFile);
-            normalVolume = getVolume();
         }
     }
     public static void playMusic(File musicFile) {
-        try {
-            System.out.println(musicFile);
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(musicFile);
-            clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Media sound = new Media(musicFile.toURI().toString());
+        backgroundMusic = new MediaPlayer(sound);
+        backgroundMusic.setVolume(normalVolume);
+        backgroundMusic.play();
     }
-    public static void fadeOutMusic(float minVolume) {
+    public static void fadeOutMusic(double minVolume) {
         //fade out music with parallel thread (to avoid pausing the game)
         new Thread(() -> {
-            float volume = getVolume();
+            double volume = getVolume();
             while (volume > minVolume) {
                 try {
                     Thread.sleep(20);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                volume -= 0.005f;
+                volume -= 0.005;
                 setVolume(volume);
             }
             System.out.println("Music faded out.");
         }).start();
     }
-    private static float getVolume() {
-        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        return (float) Math.pow(10f, gainControl.getValue() / 20f);
-    }
-    private static void setVolume(float volume) {
-        if (volume < 0f || volume > 1f)
-            return;
-        if (clip != null) {
-            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            try {
-                gainControl.setValue(20f * (float) Math.log10(volume));
-            } catch (Exception e) {
-                if (volume == 0f) {
-                    clip.stop();
-                    clip.close();
-                    clip = null;
-                }
-            }
+    private static double getVolume() {
+        if (backgroundMusic != null) {
+            return (float) backgroundMusic.getVolume();
+        } else {
+            return 0.00;
         }
     }
-    public static void getNormalVolume() {
-        setVolume(normalVolume);
+    private static void setVolume(double volume) {
+        if (volume < 0.00 || volume > 1.00)
+            return;
+        if (backgroundMusic != null) {
+            backgroundMusic.setVolume(volume);
+        }
     }
     public static void resetVolume() {
-        if (clip != null) {
+        if (backgroundMusic != null) {
+            final double[] volume = {getVolume()};
             if (normalVolume < getVolume()) {
                 new Thread(() -> {
-                    float volume = getVolume();
-                    while (volume > normalVolume) {
+                    while (volume[0] > normalVolume) {
                         try {
                             Thread.sleep(20);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        volume -= 0.01f;
-                        setVolume(volume);
+                        volume[0] -= 0.01;
+                        setVolume(volume[0]);
                     }
                     System.out.println("Music faded out.");
                 }).start();
             } else {
                 new Thread(() -> {
-                    float volume = getVolume();
-                    while (volume < normalVolume) {
+                    while (volume[0] < normalVolume) {
                         try {
                             Thread.sleep(20);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        volume += 0.01f;
-                        setVolume(volume);
+                        volume[0] += 0.01;
+                        setVolume(volume[0]);
                     }
                     System.out.println("Music faded in.");
                 }).start();
             }
         }
+    }
+    public static void resetAll() {
+        if (backgroundMusic != null) {
+            backgroundMusic.stop();
+            backgroundMusic = null;
+        }
+    }
+    public static Scene playCutScene(File videoFile) {
+        Pane pane = new Pane();
+        pane.setPrefSize(1600, 800);
+        Media media = new Media(videoFile.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+        MediaView mediaView = new MediaView(mediaPlayer);
+        pane.getChildren().add(mediaView);
+        return new Scene(pane);
+    }
+    public static void stopCutScene() {
+        if (backgroundMusic != null) {
+            backgroundMusic.stop();
+            backgroundMusic = null;
+        }
+    }
+    public static void pauseMusic() {
+        if (backgroundMusic != null) {
+            backgroundMusic.pause();
+        }
+    }
+    public static void resumeMusic() {
+        if (backgroundMusic != null) {
+            backgroundMusic.play();
+        }
+    }
+    public static void playSound(String pathFile) {
+        //create new Thread to play sound (to avoid pausing the game)
+        new Thread(() -> {
+            File soundFile = new File(pathFile);
+            Media sound = new Media(soundFile.toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.setVolume(normalVolume);
+            mediaPlayer.play();
+        }).start();
     }
 }
